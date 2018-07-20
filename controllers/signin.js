fda8f3fd6c91e4fd0c23863dd32e8167a90088fd
gogolab <m4ttek@gmail.com>
2018-07-20 16:20:03 +0200

    added redis to docker, added jwt

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,9 @@
+const jwt = require("jsonwebtoken");
+const redis = require("redis");
+
+// setup Redis:
+const redisClient = redis.createClient(process.env.REDIS_URI);
+
 const handleSignin = (req, res, db, bcrypt) => {
     const { email, password } = req.body;
 
@@ -25,13 +31,37 @@ const handleSignin = (req, res, db, bcrypt) => {
         .catch(err => Promise.reject("Unable to login"));
 };
 
+const signToken = email => {
+    const jwtPayload = { email };
+    return jwt.sign(jwtPayload, "JWT_SECRET");
+};
+
+const setToken = (key, value) => {
+    return Promise.resolve(redisClient.set(key, value));
+};
+
+const createSessions = user => {
+    // jwt token, return user data
+    const { id, email } = user;
+    const token = signToken(email);
+
+    return setToken(token, id)
+        .then(() => ({ success: true, userId: id, token: token }))
+        .catch(console.log);
+};
+
 const signinAuthentication = (db, bcrypt) => (req, res) => {
     const { authorization } = req.headers;
 
     return authorization
         ? getAuthTokenId()
         : handleSignin(req, res, db, bcrypt)
-              .then(data => res.json(data))
+              .then(data => {
+                  return data.id && data.email
+                      ? createSessions(data)
+                      : Promise.reject(data);
+              })
+              .then(session => res.json(session))
               .catch(err => res.status(400).json(err));
 };
 
